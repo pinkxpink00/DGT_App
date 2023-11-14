1099e6f9543b3ec40ecaff158b24f58db58fee1a
pinkxpink00 <kyperu07@gmail.com>
2023-11-14 23:50:52 +0200

    add service

---
+++ DGT_App/Core/Services/ApiService.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+
+namespace DGT_App.Core.Services
+{
+    public class ApiService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _apiKey;
+
+        public ApiService(HttpClient httpClient, string apiKey)
+        {
+            _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+            _apiKey = apiKey ?? throw new ArgumentNullException(nameof(apiKey));
+        }
+
+        public async Task<string> SubmitCodeAsync(string language, string sourceCode, string input, int memoryLimit, int timeLimit, string context, string callbackUrl)
+        {
+            try
+            {
+                var apiUrl = "https://api.hackerearth.com/v4/partner/code-evaluation/submissions/";
+
+                //  add a key API to a query header
+                _httpClient.DefaultRequestHeaders.Add("client-secret", _apiKey);
+
+                // POST REQUEST Body Shaping
+                var requestBody = new
+                {
+                    lang = language,
+                    source = sourceCode,
+                    input = input,
+                    memory_limit = memoryLimit,
+                    time_limit = timeLimit,
+                    context = context,
+                    callback = callbackUrl
+                };
+
+                var jsonBody = Newtonsoft.Json.JsonConvert.SerializeObject(requestBody);
+                var content = new StringContent(jsonBody, Encoding.UTF8, "application/json");
+
+                // Post query execution
+                HttpResponseMessage response = await _httpClient.PostAsync(apiUrl, content);
+
+                // Request success check
+                if (response.IsSuccessStatusCode)
+                {
+                    return await response.Content.ReadAsStringAsync();
+                }
+                else
+                {
+                    return $"Error: {response.StatusCode} - {response.ReasonPhrase}";
+                }
+            }
+            catch (Exception ex)
+            {
+                return $"Error: {ex.Message}";
+            }
+        }
+
+        public async Task<string> GetCodeStatusAsync(string submissionId)
+        {
+            try
+            {
+                var apiUrl = $"https://api.hackerearth.com/v4/partner/code-evaluation/submissions/{submissionId}/";
+
+                // To add a key API to a query header
+                _httpClient.DefaultRequestHeaders.Add("client-secret", _apiKey);
+
+                // Executing GET-Query
+                HttpResponseMessage response = await _httpClient.GetAsync(apiUrl);
+
+                // Request success check
+                if (response.IsSuccessStatusCode)
+                {
+                    return await response.Content.ReadAsStringAsync();
+                }
+                else
+                {
+                    return $"Error: {response.StatusCode} - {response.ReasonPhrase}";
+                }
+            }
+            catch (Exception ex)
+            {
+                return $"Error: {ex.Message}";
+            }
+        }
+    }
+}
---
+++ DGT_App/Core/Services/TaskService.cs
@@ -0,0 +1,80 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using DGT_App.Core.Models;
+
+namespace DGT_App.Core.Services
+{
+    public class TaskService
+    {
+        private readonly ApiService _apiService;
+
+        public TaskService(ApiService apiService)
+        {
+            _apiService = apiService ?? throw new ArgumentNullException(nameof(apiService));
+        }
+
+        // Method for executing code using the HackerEarth Open API
+        public async Task<string> ExecuteCodeAsync(string language, string sourceCode, string input, int memoryLimit, int timeLimit, string context, string callbackUrl)
+        {
+            // Prepare data for the request
+            var requestData = new
+            {
+                lang = language,
+                source = sourceCode,
+                input,
+                memory_limit = memoryLimit,
+                time_limit = timeLimit,
+                context,
+                callback = callbackUrl
+            };
+
+            // Serialize data to JSON
+            var jsonContent = JsonConvert.SerializeObject(requestData);
+            var content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+
+            // Call the method from ApiService to execute the code
+            var submissionResult = await _apiService.SubmitCodeAsync(language, sourceCode, input, memoryLimit, timeLimit, context, callbackUrl);
+
+            return submissionResult;
+        }
+
+        // Method for getting the code execution status
+        public async Task<string> GetCodeExecutionStatusAsync(string submissionId)
+        {
+            // Call the method from ApiService to get the status
+            var status = await _apiService.GetCodeStatusAsync(submissionId);
+
+            return status;
+        }
+        public async Task<TaskModel> GetTaskByIdAsync(int taskId)
+        {
+            
+            throw new NotImplementedException();
+        }
+
+       
+        public async Task<int> CreateTaskAsync(TaskModel task)
+        {
+           
+            throw new NotImplementedException();
+        }
+
+        
+        public async Task<bool> UpdateTaskAsync(int taskId, TaskModel updatedTask)
+        {
+            
+            throw new NotImplementedException();
+        }
+
+        
+        public async Task<bool> DeleteTaskAsync(int taskId)
+        {
+            
+            throw new NotImplementedException();
+        }
+    }
+
+}
